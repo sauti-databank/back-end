4e4edfe22a497d53f77fb205c909830e48f11742
Greg Jordan <gregljordan@gmail.com>
2019-09-26 09:17:11 -0400

    Added sequelize and mysql

---
+++ connector/connector.js
@@ -0,0 +1,7 @@
+const Sequelize  = require('sequelize');
+
+const sequelize = new Sequelize('mysql', 'lambda2019', 'JlgRzqmvh83Z', {
+    host: '160.153.141.46',
+    dialect: 'mysql',
+    port: 3306
+} )
