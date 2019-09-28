75a11468e8aefc830898aaeafde56931b97a619a
Nolan Allen <nolan.r.allen@gmail.com>
2019-09-27 20:26:57 -0500

    Initial commit: nothing works just yet, did some bughunting and finally got the knex bug dead

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,7 +1,9 @@
+require("dotenv").config();
+
 const knex = require("knex");
 
 const knexConfig = require("../knexfile.js");
 
-const dbEnv = process.env.DB_ENV || 'development'
+// const dbEnv = `${process.env.DB_ENV}` || 'development';
 
-module.exports = knex(knexConfig[ dbEnv ]);
+module.exports = knex(knexConfig.development);
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const server = require("./api/server");
+console.log(`This is the ${process.env.DB_ENV}`);
 
 const port = process.env.PORT || 8000;
 server.listen(port, () => {
