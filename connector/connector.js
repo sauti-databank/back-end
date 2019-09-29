ed4f8389e1a9737baf56058a89711259f99614d0
Greg Jordan <gregljordan@gmail.com>
2019-09-29 05:23:53 -0400

    Cleaning up file structure

--- connector/connector.js
+++
@@ -1,9 +0,0 @@
-const Sequelize  = require('sequelize');
-
-const sequelize = new Sequelize('mysql', 'lambda2019', 'JlgRzqmvh83Z', {
-    host: '160.153.141.46',
-    dialect: 'mysql',
-    port: 3306
-} )
-
-module.exports sequelize;
